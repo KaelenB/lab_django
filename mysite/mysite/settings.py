004bdb2b44f220aa2bea0e687117e3842387895d
KaelenB <kaelenb6@gmail.com>
2023-10-16 17:41:57 -0600

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-django_on_heroku.settings(locals()) # bottom of the file
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals()) # bottom of the file
